7e7677c63f6b222f418a274d346004e54b6be05a
BT_1N3_20 <BT_1N3_20@BT_1N3_20.bktech.local>
2017-09-14 14:46:55 -0400

    character b created

---
+++ duelingvincentndavid/src/duel/CharacterB.java
@@ -0,0 +1,31 @@
+package duel;
+
+public class CharacterB implements Dueler{
+	
+	private String name;
+	public int hp;
+	
+	public CharacterB(){
+		
+	}
+	public void taunt() {
+		String taunt = ("Fight");
+		System.out.println(taunt);
+	}
+	public String getName; {
+		
+	}
+	
+	public void setStartingHP(int hp) {
+		
+		
+	}
+	
+	public void int getHP(){
+	
+		return hp;
+		
+	}
+	
+
+}

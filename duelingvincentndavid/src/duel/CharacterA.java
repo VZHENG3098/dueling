35add393fafdcbd3c278979a5e32867f889d8394
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-09-14 14:40:49 -0400

    Updated CharacterA HP

--- duelingvincentndavid/src/duel/CharacterA.java
+++ duelingvincentndavid/src/duel/CharacterA.java
@@ -1,14 +1,24 @@
 package duel;
 
 public class CharacterA implements Dueler{
+	private String name;
+	private int hp;
+	
 	public CharacterA(){
 		
 	}
 	public void taunt() {
+		String output = ("stuff");
+		System.out.println(output);
+	}
+	public String getName() {
+		return name;
+	}
+	public void setStartingHP(int hp) {
 		
 	}
-	public String getName; {
-		
+	public int getHP() {
+		return hp;
 	}
 
 	
